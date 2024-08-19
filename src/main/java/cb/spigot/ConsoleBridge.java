cf92c3fd2eab412d84d8286bf3467abe2e26f16a
NewAmazingPVP <gamingofpat@gmail.com>
2024-08-18 20:32:34 -0400

    spigot console bridge almost done
    spigot console bridge almost done

--- src/main/java/cb/spigot/ConsoleBridge.java
+++ src/main/java/cb/spigot/ConsoleBridge.java
@@ -1,17 +1,56 @@
 package cb.spigot;
 
+import me.scarsz.jdaappender.ChannelLoggingHandler;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.EnumSet;
 
 public final class ConsoleBridge extends JavaPlugin {
+    public JDA jda;
+    public String consoleChannel;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                getServer().dispatchCommand(getServer().getConsoleSender(), "chunky continue");
+                ChannelLoggingHandler handler = new ChannelLoggingHandler(() -> jda.getTextChannelById(consoleChannel), config -> {
+                    config.setColored(true);
+                    config.setSplitCodeBlockForLinks(false);
+                    config.setAllowLinkEmbeds(true);
+                    if (isSmp) {
+                        config.addFilter(logItem -> {
+                            String message = logItem.getMessage();
+                            return message.contains("not pass event");
+                        });
+                    }
+                    config.mapLoggerName("net.dv8tion.jda", "JDA");
+                    config.mapLoggerName("net.minecraft.server.MinecraftServer", "Server");
+                    config.mapLoggerNameFriendly("net.minecraft.server", s -> "Server/" + s);
+                    config.mapLoggerNameFriendly("net.minecraft", s -> "Minecraft/" + s);
+                    config.mapLoggerName("github.scarsz.discordsrv.dependencies.jda", s -> "DiscordSRV/JDA/" + s);
+                }).attachLog4jLogging().schedule();
+                handler.schedule();
+            }
+        }.runTaskLater(this, 120L);
+    }
 
+    public  void intializeBot() {
+        String token = getConfig().getString("bot-token");
+        consoleChannel = getConfig().getString("console-channel-id");
+        //EnumSet<GatewayIntent> allIntents = EnumSet.allOf(GatewayIntent.class);
+
+        JDABuilder jdaBuilder = JDABuilder.createDefault(token);
+        //jdaBuilder.enableIntents(allIntents);
+        jda = jdaBuilder.build();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
