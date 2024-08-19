854dbff8e368ba34d95bcb9965969722c89416f1
NewAmazingPVP <gamingofpat@gmail.com>
2024-08-19 19:40:56 -0400

    console command feature added
    console command feature added

--- src/main/java/newamazingpvp/spigot/ConsoleBridge.java
+++ src/main/java/newamazingpvp/spigot/ConsoleBridge.java
@@ -1,13 +1,10 @@
-package cb.spigot;
+package newamazingpvp.spigot;
 
 import me.scarsz.jdaappender.ChannelLoggingHandler;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-
-import java.util.EnumSet;
 
 public final class ConsoleBridge extends JavaPlugin {
     public JDA jda;
@@ -15,20 +12,20 @@ public final class ConsoleBridge extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
         new BukkitRunnable() {
             @Override
             public void run() {
-                getServer().dispatchCommand(getServer().getConsoleSender(), "chunky continue");
+                intializeBot();
+                //getServer().dispatchCommand(getServer().getConsoleSender(), "chunky continue");
                 ChannelLoggingHandler handler = new ChannelLoggingHandler(() -> jda.getTextChannelById(consoleChannel), config -> {
-                    config.setColored(true);
-                    config.setSplitCodeBlockForLinks(false);
-                    config.setAllowLinkEmbeds(true);
-                    if (isSmp) {
-                        config.addFilter(logItem -> {
-                            String message = logItem.getMessage();
-                            return message.contains("not pass event");
-                        });
-                    }
+                    config.setColored(getConfig().getBoolean("colored"));
+                    config.setSplitCodeBlockForLinks(getConfig().getBoolean("split-code-block-for-links"));
+                    config.setAllowLinkEmbeds(getConfig().getBoolean("allow-link-embeds"));
+                    /*config.addFilter(logItem -> {
+                        String message = logItem.getMessage();
+                        return message.contains("not pass event");
+                    });*/
                     config.mapLoggerName("net.dv8tion.jda", "JDA");
                     config.mapLoggerName("net.minecraft.server.MinecraftServer", "Server");
                     config.mapLoggerNameFriendly("net.minecraft.server", s -> "Server/" + s);
@@ -37,7 +34,7 @@ public final class ConsoleBridge extends JavaPlugin {
                 }).attachLog4jLogging().schedule();
                 handler.schedule();
             }
-        }.runTaskLater(this, 120L);
+        }.runTaskLaterAsynchronously(this, 0L);
     }
 
     public  void intializeBot() {
@@ -48,6 +45,7 @@ public final class ConsoleBridge extends JavaPlugin {
         JDABuilder jdaBuilder = JDABuilder.createDefault(token);
         //jdaBuilder.enableIntents(allIntents);
         jda = jdaBuilder.build();
+        jda.addEventListener(new ConsoleCommand(consoleChannel, this));
     }
 
     @Override
